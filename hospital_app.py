cfac46480028c25fc228b9d1069c0b25f6d78392
PATRICIAMICHELE <patriciamichelepoliran@gmail.com>
2026-06-27 11:12:31 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -11,17 +11,17 @@ def load_model():
   with open("hospital_model.pkl", "rb" ) as f:
     return pickle.load(f)
     
-  bundle = load_model()
+bundle = load_model()
   
-  model = bundle['mode']
-  scaler = bundle['scaler']
-  features = bundle['features']
-  cols_to_scale = bundle['cols_to_scale']
-  gender_map = bundle['gender_map']
-  temp_map = bundle['temp_map']
-  hr_map = bundle['hr_map']
-  dur_map = bundle['dur_map']
-  cc_map = bundle['cc_map']
+model = bundle['mode']
+scaler = bundle['scaler']
+features = bundle['features']
+cols_to_scale = bundle['cols_to_scale']
+gender_map = bundle['gender_map']
+temp_map = bundle['temp_map']
+hr_map = bundle['hr_map']
+dur_map = bundle['dur_map']
+cc_map = bundle['cc_map']
 
 st.header("Patient Information")
 
