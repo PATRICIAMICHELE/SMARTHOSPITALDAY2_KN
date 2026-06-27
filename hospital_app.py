b24fd5e79169658b0891b31de867959a12f71582
PATRICIAMICHELE <patriciamichelepoliran@gmail.com>
2026-06-27 10:18:48 +0700

    Implement model loading function
    Added a function to load the hospital model and its components.

--- hospital_app.py
+++ hospital_app.py
@@ -5,3 +5,19 @@ import pickle
 st.set_page_config(page_title="Helps Patients Navigate Symptoms", page_icon="🏥")
 
 st.title("The Poliran Hospital🏥-navigateursymptoms😷")
+
+@st.cache_resource
+def load_model():
+  with open("hospital_model.pkl", "rb" )
+    return pickle.load(f)
+  bundle = load_model()
+  
+  model = bundle['mode']
+  scaler = bundle['scaler']
+  features = bundle['features']
+  cols_to_scale = bundle['cols_to_scale']
+  gender_map = bundle['gender_map']
+  temp_map = bundle['temp_map']
+  hr_map = bundle['hr_map']
+  dur_map = ['dur_map']
+  cc_map = ['cc_map']
